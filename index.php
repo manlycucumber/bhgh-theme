2718386f709de32e3fc5ff4f432a304368a64c7c
Manly Cucumber <manlycucumber@protonmail.com>
2023-02-16 12:53:41 -0500

    Fix Search

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
 
 						function changeOutDate(){
 							d=new Date(document.getElementById("checkout").value);
-							alert("d");
+							alert(d);
 							dt=d.getDate();
 							mn=d.getMonth();
 							mn++;
