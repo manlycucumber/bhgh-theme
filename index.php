28e0d07054d4dfa5d0bdefae44d0653fbdd72454
Manly Cucumber <manlycucumber@protonmail.com>
2023-02-16 12:30:47 -0500

    Fix Search

--- index.php
+++ index.php
@@ -14,23 +14,8 @@
 			<div class="ms-lg-4">
 				<h2>Book Your Stay</h2>
 				<br />
-				<form method="GET" class="mphb_sc_search-form" action="https://blueheron.nowhost.me/search-results/">
-					<div class="row">
-						<div class="col-xl-6 text-center">
-							<div class="input-group input-group-static mb-4">
-								<label>Check-In</label>
-								<input id="checkin" class="form-control" type="date" onblur="checkin()" >
-							</div>
-						</div>
-						<div class="col-xl-6 text-center">
-							<div class="input-group input-group-static mb-4">
-								<label>Check-Out</label>
-								<input id="checkout" class="form-control" type="date" onblur="checkout()" >
-							</div>
-						</div>
-					</div>
 
-					<script>
+				<script>
 					function checkin(){
 						d=new Date(document.getElementById("checkin").value);
 						dt=d.getDate();
@@ -47,11 +32,26 @@
 						yy=d.getFullYear();
 						document.getElementById("check_out_date").value=mn+"-"+dt+"-"+yy
 					}
-					</script>
+				</script>
+
+				<form method="GET" class="mphb_sc_search-form" action="https://blueheron.nowhost.me/search-results/">
+					<div class="row">
+						<div class="col-xl-6 text-center">
+							<div class="input-group input-group-static mb-4">
+								<label>Check-In</label>
+								<input id="checkin" class="form-control" type="date" onblur="checkin()" >
+							</div>
+						</div>
+						<div class="col-xl-6 text-center">
+							<div class="input-group input-group-static mb-4">
+								<label>Check-Out</label>
+								<input id="checkout" class="form-control" type="date" onblur="checkout()" >
+							</div>
+						</div>
+					</div>
 
 					<input id="check_in_date" name="mphb_check_in_date" type="text" hidden>
 					<input id="check_out_date" name="mphb_check_out_date" type="text" hidden>
-
 
 					<div class="row">
 						<div class="col-xl-6 text-center">
